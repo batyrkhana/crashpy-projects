8987fe729499befb729cac673156c489feca2c95
Batyrkhan Alimzhanov <144455676+batyrkhana@users.noreply.github.com>
2024-12-28 11:54:14 -0500

    Update hello_world.py

--- chapter1/hello_world.py
+++ chapter1/hello_world.py
@@ -1,4 +1,5 @@
-print("Hello python world!")
+# print("Hello python world!")
+
 """ 
 In order to run it from the terminal, you must:
 cd Documents/GitHub/crashpy-projects/
@@ -8,3 +9,9 @@ in order to look inside
 once you are in the correct repository you can run 
 python3 and write the file name after this
 """
+message = "Hello Python World"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+# you can constantly update the value for the variable message and python will keep track of the update
